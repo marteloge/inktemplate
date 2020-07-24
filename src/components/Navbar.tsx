d948f89fe4194053bf5d66b4fb6705a777d120c5
Marte Løge <marte.loge@gmail.com>
2020-07-24 15:14:38 -0400

    font display swap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,11 +32,13 @@ const Navbar = ({ t }) => (
       }
       a {
         font-family: "Raleway", sans-serif;
+        font-display: swap;
         font-size: ${calculateResponsiveSize(12, 18)};
         margin: 1.8vmin;
       }
       #logo a {
         font-family: "Playfair Display", serif;
+        font-display: swap;
         font-size: ${calculateResponsiveSize(18, 45)};
         font-weight: bold;
       }
