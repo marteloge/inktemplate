1e89f82c5bf2b1473b396dd0650f3e7ca4357e3f
Marte Løge <marte.loge@gmail.com>
2020-08-29 10:01:24 -0400

    Add dynamic sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,29 @@
+const createSitemap = (
+  host,
+  links,
+  languages
+) => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${languages
+          .map((lang) => {
+            return links.map((link) => {
+              if (lang === "en") {
+                return `<url><loc>http://${host}${link}</loc></url>`;
+              }
+              return `<url><loc>http://${host}/${lang}${link}</loc></url>`;
+            });
+          })
+          .join("")}
+    </urlset>
+    `;
+
+const Sitemap = () => {};
+
+Sitemap.getInitialProps = ({ res, req }) => {
+  res.setHeader("Content-Type", "text/xml");
+  res.write(createSitemap(req.headers.host, ["", "/about"], ["en", "no"]));
+  res.end();
+  return res;
+};
+
+export default Sitemap;
